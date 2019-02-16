b58873e20e53721560ab9018d51d29c2defae384
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2019-02-16 20:07:25 +0100

    Action on buttons

--- app/src/main/java/com/idoston/set_video_as_background/MainActivity.java
+++ app/src/main/java/com/idoston/set_video_as_background/MainActivity.java
@@ -1,19 +1,27 @@
 package com.idoston.set_video_as_background;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
     private VideoView mVideoView;
+    private Button telegram, github;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        github = (Button) findViewById(R.id.githubLoginButton);
+        telegram = (Button) findViewById(R.id.telegramLoginButton);
 
         mVideoView = (VideoView) findViewById(R.id.bgVideoView);
 
@@ -28,5 +36,27 @@ public class MainActivity extends AppCompatActivity {
                 mediaPlayer.setLooping(true);
             }
         });
+
+    }
+
+    public void gitHub(View view){
+        universalBox("Github login is pressed!");
+    }
+    public void telegramView(View view){
+        universalBox("Github login is pressed!");
+    }
+
+
+    public void universalBox(String msg){
+        AlertDialog.Builder alert_builder = new AlertDialog.Builder(this);
+        alert_builder.setMessage(msg)
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                });
+        AlertDialog alert = alert_builder.create();
+        alert.show();
     }
 }
