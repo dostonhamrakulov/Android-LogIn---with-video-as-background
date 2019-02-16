5bba13ca22ea73631b633300ae94734da4f7f864
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2019-02-16 19:26:57 +0100

    Initial comment

---
+++ app/src/main/java/com/idoston/set_video_as_background/MainActivity.java
@@ -0,0 +1,13 @@
+package com.idoston.set_video_as_background;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
