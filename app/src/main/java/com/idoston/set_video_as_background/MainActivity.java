67db0bf4240ff3e2b8a42335a357d16a0e97a7e4
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2019-02-16 19:53:26 +0100

    Playing video as background

--- app/src/main/java/com/idoston/set_video_as_background/MainActivity.java
+++ app/src/main/java/com/idoston/set_video_as_background/MainActivity.java
@@ -1,13 +1,32 @@
 package com.idoston.set_video_as_background;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private VideoView mVideoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mVideoView = (VideoView) findViewById(R.id.bgVideoView);
+
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.uzbekistan);
+
+        mVideoView.setVideoURI(uri);
+        mVideoView.start();
+
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setLooping(true);
+            }
+        });
     }
 }
